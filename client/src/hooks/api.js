498e2a0580e96f9bb10e05bcabbdd07d914984a8
Ankur0310 <ankurknp0310@gmail.com>
2023-07-07 00:57:36 +0530

    connect

--- client/src/hooks/api.js
+++ client/src/hooks/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8800'
+  baseURL: 'https://apna-nazariyaa.onrender.com'
 });
 
 export default axiosInstance;
